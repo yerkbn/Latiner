d37bc8408784404556c780c9b6e3823d0c912f24
bayshubar <yerkebulan199@gmail.com>
2020-07-08 09:22:34 +0600

    Latiner folder structure is created

---
+++ android/app/src/main/kotlin/com/example/latiner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latiner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/counter/bloc/counter_event.dart
@@ -0,0 +1,5 @@
+part of 'counter_bloc.dart';
+
+abstract class CounterEvent extends Equatable {
+  const CounterEvent();
+}
